602630161368b004b931cd9e8c9d2a4df1171f60
Yang, Shanshan <SYang@vscatny.com>
2017-05-18 13:19:30 -0400

    update

---
+++ 2017-fashionshow/main/projects/360-viewer/src/button.js
@@ -0,0 +1,6 @@
+const button = document.createElement('Button');
+const text = document.createTextNode('click');
+
+button.appendChild(text);
+
+export default button;
--- 2017-fashionshow/main/projects/360-viewer/src/index.js
+++ 2017-fashionshow/main/projects/360-viewer/src/index.js
@@ -1,5 +1,7 @@
+import button from './button.js'; 
 const app = document.createElement('main');
 const text = document.createTextNode('360-viewer');
 
 app.appendChild(text);
+app.appendChild(button);
 document.getElementById('root').appendChild(app);
